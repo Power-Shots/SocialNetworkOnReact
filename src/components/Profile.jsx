7b61e7d118f302b0870b82e601e6091c500c8de6
Alex <powershot317@gmail.com>
2021-09-17 14:58:29 +0300

    push on github

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__340.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
