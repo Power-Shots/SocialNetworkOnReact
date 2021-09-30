de102fb51cce4d1e840283c9b521944a1f0a9cd9
Alex <powershot317@gmail.com>
2021-09-30 14:06:43 +0300

    урок 18: страница Dialogs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -9,7 +10,12 @@ function App() {
     <div className="app-wrapper">
       <Header/>
       <Navbar/>
-      <Profile/>
+      <main className="app-wrapper-content">
+        {/* <Profile/> */}
+        <Dialogs/>
+      </main>
+      
+      
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__340.jpg" />
       </div>
