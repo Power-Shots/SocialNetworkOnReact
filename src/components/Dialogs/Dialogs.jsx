c0cfda42ef754827eb393c5dea5f8925d5e6e5d1
Alex <powershot317@gmail.com>
2021-10-27 21:36:43 +0300

    урок 21: вёрстка Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,24 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.activeDialog}>
+          Vacya
+        </div>
+      
+        <div className={s.dialog}>
+          Petya
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>Good bey</div>
+      </div>
     </div>
   )
 }
